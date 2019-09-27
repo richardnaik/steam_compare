4760cd1bc17e0242e700d882a41df7b123b6cfa3
Richard Naik <richardnaik@gmail.com>
2019-09-26 22:02:39 -0500

    Updating for python 3

--- compare.py
+++ compare.py
@@ -3,7 +3,7 @@
 import sys
 import requests
 import json
-from sets import Set
+#from sets import Set
 
 steamid_1 = sys.argv[1]
 steamid_2 = sys.argv[2]
@@ -13,14 +13,15 @@ r1 = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v000
 library_1 = []
 for element in r1.json()["response"]["games"]:
     library_1.append(element["name"])
-library_1 = Set(library_1)
+library_1 = set(library_1)
 
 r2 = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+api_key+'&steamid='+steamid_2+'&format=json&include_appinfo=1')
 library_2 = []
 for element in r2.json()["response"]["games"]:
     library_2.append(element["name"])
-library_2 = Set(library_2)
+library_2 = set(library_2)
 
 common_games = library_1.intersection(library_2)
 for element in sorted(common_games):
-    print element
+    print(element)
+
